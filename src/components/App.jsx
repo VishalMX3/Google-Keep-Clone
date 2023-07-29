93f462cf0ef31c0617614dacab24e678ef2bc3e8
Vishal Pundhir <63143819+VishalMX3@users.noreply.github.com>
2023-07-29 23:11:56 +0530

    Created a new component createArea

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,21 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <>
       <Header />
-      <Footer />
+      <CreateArea />
+     
       {notes.map(note => (
       <Note 
         key={note.key}
         title={note.title}
         content={note.content} />
     ))}
+     <Footer />
       
     </>
   );
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function CreateArea() {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          value={note.title}
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+        />
+        <textarea
+          value={note.content}
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
